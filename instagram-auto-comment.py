5740ecbd0eee165126a47402a0975c455f430d5e
gsak3l <gsakel5@gmail.com>
2020-12-26 23:44:59 +0200

    checking info should work

--- instagram-auto-comment.py
+++ instagram-auto-comment.py
@@ -1,18 +1,30 @@
 import multiprocessing
 import time
 import random
+import validators
+
 from selenium import webdriver
 
+
+def check_info(c_username, c_password, c_instagram_post_url):
+    # username stuff
+    c_username = input("username: ")
+    while not validators.url("https://www.instagram.com/" + str(c_username) + "/"):
+        c_username = input("incorrect username, try again: ")
+    u_flag = False
+    # password stuff
+    c_password = input("password: ")
+    while not len(c_password) > 6:
+        input("incorrect password will not work, try again: ")
+    # instagram post stuff
+    c_instagram_post_url = input("post url: ")
+    while not validators.url("https://www.instagram.com/" + str(c_instagram_post_url) + "/"):
+        c_instagram_post_url = input("this doesn't seem right, try again: ")
+
+
 if __name__ == '__main__':
-    flag = True
-    while flag:
-        username = input("username: ")
-
-        password = input("password: ")
-        instagram_post_url = input("instagram post url: ")
-
-
-    # driver
-    comment_spammer = webdriver.Chrome(executable_path='./chromedriver')
-    comment_spammer.get(instagram_post_url)
-
+    username = password = instagram_post_url = ''
+    check_info(username, password, instagram_post_url)
+    # comment_spammer = webdriver.Chrome(executable_path='./chromedriver')
+    # comment_spammer.get(instagram_post_url)
+    print(username, password, instagram_post_url)
