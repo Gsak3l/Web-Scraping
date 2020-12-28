6e7da07503b820fd1bcfa584e36de6c22c954395
gsak3l <gsakel5@gmail.com>
2020-12-28 11:57:14 +0200

    getting all the li's from all the followers

--- instagram-auto-comment.py
+++ instagram-auto-comment.py
@@ -17,7 +17,7 @@ def check_info():
     # password stuff
     credentials_2[1] = input("password: ")
     while not len(credentials_2[1]) > 6:
-        input("incorrect password will not work, try again: ")
+        credentials_2[1] = input("incorrect password will not work, try again: ")
 
     # instagram post stuff
     credentials_2[2] = input("post url: ")
@@ -29,6 +29,18 @@ def check_info():
 def comment(credentials_2):
     comment_spammer = webdriver.Chrome(executable_path='./chromedriver')
     comment_spammer.get(credentials_2[2])  # reaching the post
+
+    # cookie button
+    flag = True
+    while flag:
+        buttons = comment_spammer.find_elements_by_tag_name('button')
+        try:
+            for buttons in buttons:
+                if buttons.text == 'Accept':
+                    buttons.click()
+                    flag = False
+        except:
+            pass
 
     # loggin in
     flag = True
@@ -43,7 +55,7 @@ def comment(credentials_2):
             pass
     comment_spammer.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div/form/div[1]/div[3]/button').click()
 
-    # bypassing the save credentials step
+    # bypassing the save credentials step<
     while not flag:
         buttons = comment_spammer.find_elements_by_tag_name('button')
         for button in buttons:
@@ -53,9 +65,27 @@ def comment(credentials_2):
                     flag = True
             except:
                 pass
-    time.sleep(25)
 
-    # optional credential button
+    names = get_follower_names(comment_spammer, credentials_2)
+
+
+def get_follower_names(follower_names_getter, credentials_2):
+    names_2 = []
+    follower_names_getter.get('https://www.instagram.com/' + str(credentials_2[0]))
+    # getting the number of followers
+    number_of_followers = follower_names_getter.find_elements_by_tag_name('li')[1].find_element_by_tag_name('span').text
+    follower_names_getter.find_elements_by_tag_name('li')[1].click()
+    time.sleep(1)
+
+    follower_window = follower_names_getter.find_element_by_xpath('/html/body/div[5]/div/div/div[2]')
+    lis = []
+    while int(number_of_followers) - 1 >= len(lis):
+        follower_names_getter.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', follower_window)
+
+        lis = follower_names_getter.find_elements_by_tag_name('li')
+
+    print(len(lis))
+    return names_2
 
 
 if __name__ == '__main__':
